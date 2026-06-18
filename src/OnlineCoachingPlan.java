1681488778c4979d0665a9e993ab57a86e033771
GregMarkitan <s26387@pjwstk.edu.pl>
2026-06-18 13:11:37 +0200

    Implemented fields and constructor for OnlineCoachingPlan.java

---
+++ src/OnlineCoachingPlan.java
@@ -0,0 +1,11 @@
+public class OnlineCoachingPlan extends MembershipPlan implements RemoteAccess {
+	private int videoConsultations;
+	private boolean mealPlanIncluded;
+	private boolean recordedLibraryAccess;
+
+	public OnlineCoachingPlan(String planCode, String clientName, int months, double baseMonthlyFee, boolean autoRenew, int videoConsultations, boolean mealPlanIncluded, boolean recordedLibraryAccess) {
+		super(planCode, clientName, months, baseMonthlyFee, autoRenew);
+		this.videoConsultations = videoConsultations;
+		this.mealPlanIncluded = mealPlanIncluded;
+		this.recordedLibraryAccess = recordedLibraryAccess;
+   }
