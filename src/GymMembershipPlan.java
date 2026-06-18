f8af327a9ee7699980961ec49f8aa26186ebec83
GregMarkitan <s26387@pjwstk.edu.pl>
2026-06-18 12:46:06 +0200

    Initialized fields and create constructor using super() for GymMembershipPlan.java

---
+++ src/GymMembershipPlan.java
@@ -0,0 +1,9 @@
+public class GymMembershipPlan extends MembershipPlan implements Freezable {
+	private int entriesPerMonth;
+	private boolean saunaAccess;
+
+	public GymMembershipPlan(String planCode, String clientName, int months, double baseMonthlyFee, boolean autoRenew, int entriesPerMonth, boolean saunaAccess) {
+		super(planCode, clientName, months, baseMonthlyFee, autoRenew);
+		this.entriesPerMonth = entriesPerMonth;
+		this.saunaAccess = saunaAccess;
+}
