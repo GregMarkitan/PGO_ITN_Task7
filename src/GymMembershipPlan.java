2d36c81fbaa90fb74f7bc8f7a8ab43d979fc090e
GregMarkitan <s26387@pjwstk.edu.pl>
2026-06-18 12:58:13 +0200

    Implemented getPlanType(), the prising formula for the monthly net price, and the canFreeze()

--- src/GymMembershipPlan.java
+++ src/GymMembershipPlan.java
@@ -6,4 +6,32 @@ public class GymMembershipPlan extends MembershipPlan implements Freezable {
 		super(planCode, clientName, months, baseMonthlyFee, autoRenew);
 		this.entriesPerMonth = entriesPerMonth;
 		this.saunaAccess = saunaAccess;
+	@Override
+	public String getPlanType() {
+		return "Gym Membership";
+	}
+
+	@Override
+	public double calculateMonthlyNetPrice() {
+		double price = getBaseMonthlyFee();
+
+		price += entriesPerMonth * 4;
+
+		if (saunaAccess) {
+			price += 25;
+		}
+
+		if (isAutoRenew()) {
+			price -= 10;
+		}
+
+		return price;
+	}
+
+	@Override
+	public boolean canFreeze() {
+		return getMonths() >= 3;
+	}
+
+	
 }
